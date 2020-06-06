90662e50e9888ebb8513238550ed63dd0c223335
Christopher Cluck <christopher.cluck@orange.fr>
2020-06-06 19:31:38 +0200

    Bugfix: missing space after number & \n at end of echos

--- php/exercises/Luhn/LuhnTest.php
+++ php/exercises/Luhn/LuhnTest.php
@@ -4,15 +4,15 @@ require __DIR__ . '/Luhn.php';
 
 // Test good format & valid number
 $validNumber = '4539 1488 0343 6467';
-echo "Test good format & valid number [{$validNumber}]" . textResult(Luhn::isValid($validNumber));
+echo "Test good format & valid number [{$validNumber}] " . textResult(Luhn::isValid($validNumber)) . "\n";
 
 // Test good format & invalid number
 $invalidNumber = '8273 1232 7352 0569';
-echo "Test good format & invalid number [{$invalidNumber}]" . textResult(Luhn::isValid($invalidNumber));
+echo "Test good format & invalid number [{$invalidNumber}] " . textResult(Luhn::isValid($invalidNumber)) . "\n";
 
 // Test wrong format
 $wrongFormat = '4539 1z88 0343 6467';
-echo "Test wrong format [{$wrongFormat}]" . textResult(Luhn::isValid($wrongFormat));
+echo "Test wrong format [{$wrongFormat}] " . textResult(Luhn::isValid($wrongFormat)) . "\n";
 
 function textResult($result)
 {
